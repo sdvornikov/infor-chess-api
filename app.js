ad445251319475e526b3b23c99433545eadca512
Sergey <sergey.dvornikov@gmail.com>
2017-07-16 13:01:55 -0400

    basic project outline and a standard node.js .gitignore

---
+++ app.js
@@ -0,0 +1,10 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+
+const app            = express();
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log('Listening on ' + port);
+});
