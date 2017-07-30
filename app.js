bd13341dc4e4b4b8e4eb848feb430f81feb48504
Sergey <sergey.dvornikov@gmail.com>
2017-07-30 15:47:09 -0400

    simple CORS support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator')
 const dbConfig = require('./config/db');
+var cors = require('cors')
 
 const app = express();
 
@@ -10,6 +11,7 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
+app.use(cors())
 
 MongoClient.connect(dbConfig.url, (err, database) => {
   if (err) return console.log(err)
